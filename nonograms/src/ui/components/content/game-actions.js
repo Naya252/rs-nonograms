12e75906f7cca9667555cbb1b5fade0d244b353a
Naya252 <naya252naya@gmail.com>
2024-01-24 22:30:15 +0300

    feat: reset game

--- nonograms/src/ui/components/content/game-actions.js
+++ nonograms/src/ui/components/content/game-actions.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-restricted-syntax */
 import createElement from '../../../shared/helpers';
-import Timer from './timer';
+import Grid from './grid';
 
-export default class Actions extends Timer {
+export default class Actions extends Grid {
   constructor() {
     super();
     this.save = {
@@ -27,17 +28,38 @@ export default class Actions extends Timer {
     this.reset.el.removeAttribute('disabled');
   }
 
+  resetGame() {
+    this.cleanTimer();
+    this.createTimer();
+
+    this.points.cur = [];
+
+    for (const cell of this.grid.items) {
+      if (cell.classList.contains('black')) {
+        cell.classList.remove('black');
+      }
+    }
+
+    this.addDisabled();
+  }
+
+  addDisabled() {
+    this.save.el.setAttribute('disabled', true);
+    this.solution.el.setAttribute('disabled', true);
+    this.reset.el.setAttribute('disabled', true);
+  }
+
   createActions() {
     this.save.el = createElement(this.save.tag, this.save.className);
     this.save.el.innerText = this.save.className.toUpperCase();
-    this.save.el.setAttribute('disabled', true);
 
     this.solution.el = createElement(this.solution.tag, this.solution.className);
     this.solution.el.innerText = this.solution.className.toUpperCase();
-    this.solution.el.setAttribute('disabled', true);
 
     this.reset.el = createElement(this.reset.tag, this.reset.className);
     this.reset.el.innerText = this.reset.className.toUpperCase();
-    this.reset.el.setAttribute('disabled', true);
+    this.reset.el.addEventListener('click', () => this.resetGame());
+
+    this.addDisabled();
   }
 }
