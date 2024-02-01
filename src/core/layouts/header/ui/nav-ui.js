cca50197221ec49011ecad6fe299ca4dfdd2dd42
Naya252 <naya252naya@gmail.com>
2024-02-01 20:44:28 +0300

    feat: add logo & favicon

--- src/core/layouts/header/ui/nav-ui.js
+++ src/core/layouts/header/ui/nav-ui.js
@@ -1,4 +1,5 @@
 import { BaseClass, createElement } from '../../../shared/helpers';
+import { BASE_URL } from '../../../shared/constants';
 
 export default class NavUI extends BaseClass {
   constructor(tag = 'nav', className = 'container-xxl flex-wrap flex-md-nowrap') {
@@ -11,10 +12,15 @@ export default class NavUI extends BaseClass {
   create() {
     super.create();
 
+    const logo = createElement('img', 'logo');
+    logo.setAttribute('src', `${BASE_URL}logo.svg`);
+    logo.setAttribute('alt', `Logo`);
+
     const title = createElement('h1', 'navbar-brand');
     title.setAttribute('data-bs-theme', 'dark');
     title.innerText = 'Nonograms';
 
+    this.el.append(logo);
     this.el.append(title);
   }
 }
